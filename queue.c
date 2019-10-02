a00fba5c7f7fd98117d8e43906b3627312a22430
henry0929016816 <f74032112@mail.ncku.edu.tw>
2019-10-03 02:43:16 +0800

    Tetify q_new malloc is not working case

--- queue.c
+++ queue.c
@@ -26,6 +26,9 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* What if malloc returned NULL? */
+    if (q == NULL) {
+        return NULL;
+    }
     q->head = NULL;
     q->tail = NULL;
     q->size = 0;
